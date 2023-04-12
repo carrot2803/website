e6d94b5edae240b7667d4d0a38a15eecc00303e6
carrot2803 <lezamadmitri@gmail.com>
2023-04-11 21:17:54 -0400

    test files no longer needed

--- script.js
+++
@@ -1,6 +0,0 @@
-const commentBtn = document.getElementById("comment-btn");
-const commentBox = document.getElementById("comment-box");
-
-commentBtn.addEventListener("click", () => {
-  commentBox.style.display = "block";
-});
